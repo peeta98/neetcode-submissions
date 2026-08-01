ff9728354e6cbdce749ce41eabdb1dde682a2fce
Pedro Moreira <119496257+peeta98@users.noreply.github.com>
2026-08-01 13:08:57 +0000

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 28
+float_type = 2374.44
+bool_type = True
+string_type = "Pedro"
+list_type = [16, 14]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
