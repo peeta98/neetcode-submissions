b3eff1436bb452015b41e7865c897273b547353e
Pedro Moreira <119496257+peeta98@users.noreply.github.com>
2026-08-01 13:11:08 +0000

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 28
+print(type(variable))
+variable = 2354.17
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Pedro"
+print(type(variable))
+variable = [16, 14]
+print(type(variable))
