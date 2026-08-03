96b5bce5d60b91a4e98971b36f0b0b949f140707
Pedro Moreira <119496257+peeta98@users.noreply.github.com>
2026-08-03 13:05:42 +0000

    Add: python-error-catching - submission-0

---
+++ Python For Beginners/python-error-catching/submission-0.py
@@ -0,0 +1,12 @@
+def divide_numbers(a: str, b: str) -> None:
+    try:
+        num_a = int(a)
+        num_b = int(b)
+        print(num_a / num_b)
+    except Exception as error:
+        print("An error occurred:", error)
+
+# do not modify below this line
+divide_numbers("10", "2")
+divide_numbers("12", "0")
+divide_numbers("2", "not a number")
